aad21291ad7c1e87274e5cbace1b84f002b51ec4
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-08 16:00:13 +0300

    фикс роутинга для gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./fonts/fonts.css";
 import "./components/ui/common.css";
 import "./components/ui/box.css";
 import App from "./components/app/app";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
@@ -13,8 +12,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
