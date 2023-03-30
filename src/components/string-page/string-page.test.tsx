9fc0a4fb4919051c7b8a73635d8a4c3596e73b4a
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-30 19:31:32 +0300

    тесты для stringPage

--- src/components/string-page/string-page.test.tsx
+++ src/components/string-page/string-page.test.tsx
@@ -1,15 +1,68 @@
 import { StringPage } from "./string-page";
-import { render, screen, waitFor, fireEvent} from "@testing-library/react";
-import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 
-describe("Корректный разворот строки", () => {
-    it("проверяет корректный разворот строки", async () => {
-        render(
-            <MemoryRouter>
-                <StringPage />
-            </MemoryRouter>
-        );
+const testEvenString = "helloworld";
+const testOddString = "hello";
+const testOneString = "h";
 
-    });
-})
+
+describe("Корректный разворот строки", () => {
+  it("с чётным количеством символов", async () => {
+    render(
+      <MemoryRouter>
+        <StringPage />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByTestId("input");
+    const button = screen.getByTestId("button");
+
+    userEvent.type(input, testEvenString);
+    expect(input).toHaveValue(testEvenString);
+        userEvent.click(button)
+    await waitFor(() => {
+      const circles = screen.getAllByTestId("testCircle").map((item) => item.textContent);
+      expect(circles.join("")).toBe(Array(testEvenString).reverse().join(""));
+    }, { timeout: 1000 });
+  });
+
+  it("с нечётным количеством символов", async () => {
+    render(
+      <MemoryRouter>
+        <StringPage />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByTestId("input");
+    const button = screen.getByTestId("button");
+
+    userEvent.type(input, testOddString);
+    expect(input).toHaveValue(testOddString);
+        userEvent.click(button)
+    await waitFor(() => {
+      const circles = screen.getAllByTestId("testCircle").map((item) => item.textContent);
+      expect(circles.join("")).toBe(Array(testOddString).reverse().join(""));
+    }, { timeout: 1000 });
+  });
+
+  it("с одним символом", async () => {
+    render(
+      <MemoryRouter>
+        <StringPage />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByTestId("input");
+    const button = screen.getByTestId("button");
+
+    userEvent.type(input, testOneString);
+    expect(input).toHaveValue(testOneString);
+    userEvent.click(button)
+    await waitFor(() => {
+      const circles = screen.getAllByTestId("testCircle").map((item) => item.textContent);
+      expect(circles.join("")).toBe(testOneString);
+    }, { timeout: 1000 });
+  });
+});
