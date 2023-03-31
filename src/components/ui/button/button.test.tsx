1f61ab9c578b181184b5ea604f8d73da1f5a892d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-31 14:29:22 +0300

    тесты для button

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -1,9 +1,22 @@
-import TestRenderer from 'react-test-renderer';
-import { Button } from './button';
+import TestRenderer from "react-test-renderer";
+import { Button } from "./button";
 
-describe('Корректная работа Button', () => {
-    it('Проверяет корректность работы Button', () => {
-        const button = TestRenderer.create(<Button />);
-        // expect(button).toMatchSnapshot();
-    });
-})
+describe("Корректная работа Button", () => {
+  it("Button c текстом", () => {
+    const button = TestRenderer.create(<Button text={"test"} />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+
+  it("Button без текста", () => {
+    const button = TestRenderer.create(<Button />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it("Button disabled", () => {
+    const button = TestRenderer.create(<Button disabled />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it("Button disabled", () => {
+    const button = TestRenderer.create(<Button isLoader = {true} />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+});
