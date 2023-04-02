349ea1c2da95157d0e71a7dd76a30a7e3d749161
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-02 19:20:22 +0300

    кнопка отключена при пустом инпуте fibonacci

--- cypress/e2e/fibonacci-page.spec.cy.js
+++ cypress/e2e/fibonacci-page.spec.cy.js
@@ -1,7 +1,12 @@
+import { testUrl } from "../constants/constants";
 
 describe('Страница "Строка" отображается правильно', () => {
 	beforeEach(() => {
 		cy.visit(testUrl);
-		cy.get('[href*="/fibonacci"]').click()
+		cy.get('[href*="/fibonacci"]').click();
 	})
+    it('Кнопка отключена при пустом инпуте', () => {
+		cy.get("input").should('have.value', '');
+		cy.get("button").should("be.disabled")
+	});
 })
--- cypress/e2e/string-page.spec.cy.js
+++ cypress/e2e/string-page.spec.cy.js
@@ -1,37 +1,42 @@
-import { testUrl, dataTestCircle, dataTestDefault ,dataTestChanging, dataTestModified} from "../constants/constants";
+import {
+  testUrl,
+  dataTestCircle,
+  dataTestDefault,
+  dataTestChanging,
+  dataTestModified,
+} from "../constants/constants";
 
 describe('Страница "Строка" отображается правильно', () => {
-	beforeEach(() => {
-		cy.visit(testUrl);
-		cy.get('[href*="/recursion"]').click()
-	})
-	it('Кнопка отключена при пустом инпуте', () => {
-		cy.get("input").should('have.value', '');
-		cy.get("button").should("be.disabled")
-	});
+  beforeEach(() => {
+    cy.visit(testUrl);
+    cy.get('[href*="/recursion"]').click();
+  });
+  it("Кнопка отключена при пустом инпуте", () => {
+    cy.get("input").should("have.value", "");
+    cy.get("button").should("be.disabled");
+  });
 
-	it('Строка разворачивается корректно', () => {
-		cy.get("input").type('12345678');
-		cy.get("button").eq(1).click();
+  it("Строка разворачивается корректно", () => {
+    cy.get("input").type("12345678");
+    cy.get("button").eq(1).click();
 
-		cy.get(dataTestCircle)
-            .should('have.length', 8)
-            .each((el, index) => {
-                cy.wrap(el => expect(el).contains(index + 1));
-                if (index === 0 || index === 7) {
-                    cy.wrap(el).get(dataTestChanging);
-                }
-                if (index === 1) {
-                    cy.wrap(el).get(dataTestDefault);
-                }
-            });
+    cy.get(dataTestCircle)
+      .should("have.length", 8)
+      .each((el, index) => {
+        cy.wrap((el) => expect(el).contains(index + 1));
+        if (index === 0 || index === 7) {
+          cy.wrap(el).get(dataTestChanging);
+        }
+        if (index === 1) {
+          cy.wrap(el).get(dataTestDefault);
+        }
+      });
 
-        cy.get(dataTestCircle)
-            .should('have.length', 8)
-            .each((el, index) => {
-                cy.wrap(el).contains(8 - index);
-                cy.wrap(el).get(dataTestModified);
-            });
-	});
-
-}); 
+    cy.get(dataTestCircle)
+      .should("have.length", 8)
+      .each((el, index) => {
+        cy.wrap(el).contains(8 - index);
+        cy.wrap(el).get(dataTestModified);
+      });
+  });
+});
