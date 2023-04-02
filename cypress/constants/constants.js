8de72a038dca581e02c098655d4371bc9c398275
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-04-02 21:00:23 +0300

    тест правильности удаления элементов из stack-page

--- cypress/constants/constants.js
+++ cypress/constants/constants.js
@@ -4,6 +4,9 @@ export const dataTestDefault = '[class*=circle_default]';
 export const dataTestChanging = '[class*=circle_changing]';
 export const dataTestModified = '[class*=circle_modified]'
 export const dataTestCircle = '[data-testid="circleDiv"]';
+export const dataTestContentCircle = '[class*=circle_content]';
 
 export const dataTestInput = '[data-testid="input"]';
 export const dataTestInputIndex = '[data-testid="input-index"]'
+
+export const testArray = [1, 2, 3, 4]
--- cypress/e2e/stack-page.spec.cy.js
+++ cypress/e2e/stack-page.spec.cy.js
@@ -1,28 +1,62 @@
 import {
-    testUrl,
-    dataTestCircle,
-    dataTestDefault,
-    dataTestChanging,
-    dataTestModified,
-  } from "../constants/constants";
-  
-  describe('Страница "Стек" отображается правильно', () => {
-    beforeEach(() => {
-      cy.visit(testUrl);
-      cy.get('[href*="/stack"]').click();
-      cy.contains("button", "Добавить").as("addButton")
-      cy.contains("button", "Удалить").as("deleteButton")
-      cy.contains("button", "Очистить").as("clearButton")
-    });
-    it("Кнопки отключены при пустом инпуте", () => {
-      cy.get("input").should("have.value", "");
-      cy.get("@addButton").should("be.disabled")
-      cy.get("@deleteButton").should("be.disabled")
-      cy.get("@clearButton").should("be.disabled")
-    });
-  
-    it("Строка разворачивается корректно", () => {
-    
-    });
+  testUrl,
+  dataTestCircle,
+  dataTestDefault,
+  dataTestChanging,
+  dataTestModified,
+  testArray,
+  dataTestContentCircle,
+} from "../constants/constants";
+
+describe('Страница "Стек" отображается правильно', () => {
+  beforeEach(() => {
+    cy.visit(testUrl);
+    cy.get('[href*="/stack"]').click();
+    cy.contains("button", "Добавить").as("addButton");
+    cy.contains("button", "Удалить").as("deleteButton");
+    cy.contains("button", "Очистить").as("clearButton");
   });
-  
+  it("Кнопки отключены при пустом инпуте", () => {
+    cy.get("input").should("have.value", "");
+    cy.get("@addButton").should("be.disabled");
+    cy.get("@deleteButton").should("be.disabled");
+    cy.get("@clearButton").should("be.disabled");
+  });
+
+  it("Проверка правильности добавления элемента в стек", () => {
+    for (let i = 0; i < testArray.length; i++) {
+      cy.clock();
+
+      cy.get("input").should("be.empty").type(testArray[i]);
+      cy.get("@addButton").should("not.be.disabled").click();
+      cy.get(dataTestCircle);
+      cy.tick(500);
+
+      cy.get(dataTestCircle).eq(i).should("have.text", testArray[i]);
+      cy.get(dataTestContentCircle)
+        .eq(i)
+        .find(dataTestChanging)
+        .parent()
+        .should("contain", "top");
+      cy.tick(500);
+
+      cy.get(dataTestContentCircle).eq(0).find(dataTestDefault);
+    }
+  });
+  it("Правильность удаления элемента из стека", () => {
+    for (let i = 0; i < testArray.length; i++) {
+      cy.get("input").should("be.empty").type(testArray[i]);
+      cy.get("@addButton").should("not.be.disabled").click();
+    }
+    cy.get(dataTestCircle).as("circles");
+    for (let j = 0; j <= testArray.length; j++) {
+      cy.get("@deleteButton").click();
+      cy.get("body").then(($body) => {
+        if ($body.text().includes(dataTestCircle)) {
+          cy.get(dataTestCircle).eq(testArray.length - 1 - j);
+          cy.get(dataTestContentCircle).eq(1).find(dataTestChanging);
+        }
+      });
+    }
+  });
+});
